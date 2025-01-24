a3d0fb7755be5509aa802a5411f36d3ffc29fa8a
Asim <asimmahmood11110@gmail.com>
2025-01-24 14:59:56 +0500

    Spinner added

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
   
     const stickySection = document.querySelector(".steps");
     const stickySectionb = document.querySelector(".outro");
-    const stickyHeight = 1000;
+    const stickySectionc = document.querySelector(".hero");
+    const stickyHeight = 4500;
+    const stickyHeightb = 1000;
+    const stickyHeightc = 2500;
     const cards = document.querySelectorAll(".card");
     const countContainer = document.querySelector(".count-container");
     const totalCards = cards.length;
@@ -28,13 +31,93 @@ document.addEventListener("DOMContentLoaded", () => {
     ScrollTrigger.create({
       trigger: stickySectionb,
       start: "top top",
-      end: `+=${stickyHeight}px`,
+      end: `+=${stickyHeightb}px`,
       pin: true,
-      pinSpacing: true,
-      onUpdate: (self) => {
-        positionCards(self.progress);
+      pinSpacing: true
+    });
+
+    ScrollTrigger.create({
+      trigger:".pinned",
+      start: "top top",
+      endTrigger: ".whitespace",
+      end: "bottom top",
+      pin: true,
+      pinSpacing: false
+    });
+
+    ScrollTrigger.create({
+      trigger:".header-info",
+      start: "top top",
+      endTrigger: ".whitespace",
+      end: "bottom top",
+      pin: true,
+      pinSpacing: false
+    });
+
+    ScrollTrigger.create({
+      trigger:".pinned",
+      start: "top top",
+      endTrigger: ".header-info",
+      end: "bottom bottom",
+      onUpdate: (self)=>{
+        const rotation = self.progress * 360;
+        gsap.to(".revealer", {rotation});
+      }
+    });
+
+    ScrollTrigger.create({
+      trigger:".pinned",
+      start: "top top",
+      endTrigger: ".header-info",
+      end: "bottom bottom",
+      onUpdate: (self)=>{
+        const progress = self.progress;
+        const clipPath = `polygon(
+          ${45 - 45 * progress}% ${0 + 0 * progress}%,
+          ${55 + 45 * progress}% ${0 + 0 * progress}%,
+          ${55 + 45 * progress}% ${100 - 0 * progress}%,
+          ${45 - 45 * progress}% ${100 - 0 * progress}%
+        )`;
+        gsap.to(".revealer-1, .revealer-2",{
+          clipPath : clipPath,
+          ease: "none",
+          duration: 0,
+        });
       },
     });
+
+    ScrollTrigger.create({
+      trigger:".header-info",
+      start: "top top",
+      end: "bottom 50%",
+      scrub: 1,
+      onUpdate: (self)=>{
+        const progress = self.progress;
+        const left = 35 + 15 * progress;
+        gsap.to(".revealer",{
+          left: `${left}%`,
+          ease: "none",
+          duration: 0
+        });
+      },
+    });
+
+    ScrollTrigger.create({
+      trigger:".whitespace",
+      start: "top 50%",
+      end: "bottom bottom",
+      scrub: 1,
+      onUpdate: (self)=>{
+        const scale = 1 + 12 * self.progress;
+        gsap.to(".revealer",{
+          scale: scale,
+          ease: "none",
+          duration: 0,
+        });
+      },
+    });
+
+
   
     const getRadius = () => {
       return window.innerWidth < 900
